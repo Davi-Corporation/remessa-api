44df26355aeb132ca495a65a3ff8c4bd27c71131
Davi Bariani Boin <dbb2000@gmail.com>
2026-08-05 16:55:52 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/api/remessa/RemessaApplication.java
@@ -0,0 +1,13 @@
+package com.api.remessa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemessaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemessaApplication.class, args);
+	}
+
+}
